615e4d31f886ec07224a037c54e9271abc9de7d9
Denis Sigaev <denis.sigaev@asml.com>
2019-07-10 17:01:50 +0200

    Tackling with r=1 case Fixing multiple errors Only one test case is failing

--- src/Triplets.java
+++ src/Triplets.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toList;
 
 public class Triplets {
-    static class Number {
+    private static class Number {
         Long base;
         Integer rPower;
 
@@ -15,37 +15,40 @@ public class Triplets {
         }
     }
 
+    private static class ValueCount {
+        Integer powNum;
+        Integer tuplesNum;
+
+        ValueCount(){
+            this.powNum = 0;
+            this.tuplesNum = 0;
+        }
+    }
+
     private static Map<Long, Number> valuesCache = new HashMap<>();
 
     // Complete the countTriplets function below.
     private static long countTriplets(List<Long> arr, long r) {
-        Map<Long, List<Number>> triplets = new HashMap<>();
-
-        for (Long aLong : arr) {
-            Number triplet = getNumber(aLong, r);
-            List<Number> listForBase = triplets.computeIfAbsent(triplet.base, (k) -> new ArrayList<>());
-            listForBase.add(triplet);
+        if(r == 1L){
+            return countTripletsWhenBase1(arr);
         }
 
         long totalNumTriplets = 0;
-        for (Map.Entry<Long, List<Number>> entry : triplets.entrySet()) {
-            int size = entry.getValue().size();
-            if (size < 3) {
-                continue;
+        Map<Long, ValueCount[]> triplets = new HashMap<>();
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            Number number = getNumber(arr.get(i), r);
+            ValueCount[] numberOfPowers = triplets.computeIfAbsent(number.base, k -> new ValueCount[30]);
+            ValueCount powerValue = numberOfPowers[number.rPower];
+            if(powerValue == null){
+                powerValue = new ValueCount();
+                numberOfPowers[number.rPower] = powerValue;
             }
-            Map<Integer, Integer> numberOfPowers = new HashMap<>();
-            Map<Integer, Integer> numberOfTuples = new HashMap<>();
-            for (int i = size - 1; i >= 0; i-- ) {
-                int power = entry.getValue().get(i).rPower;
-                numberOfPowers.merge(power, 1, Integer::sum);
-                Integer powerPlus1 = numberOfPowers.get(power + 1);
-                if(powerPlus1 != null) {
-                    numberOfTuples.merge(power, powerPlus1, Integer::sum);
-                    Integer tuplesPlus1 = numberOfTuples.get(power + 1);
-                    if(tuplesPlus1 != null){
-                        totalNumTriplets += tuplesPlus1;
-                    }
-                }
+            powerValue.powNum++;
+
+            ValueCount powerPlus1Value = numberOfPowers[number.rPower + 1];
+            if(powerPlus1Value != null) {
+                powerValue.tuplesNum += powerPlus1Value.powNum;
+                totalNumTriplets += powerPlus1Value.tuplesNum;
             }
         }
         return totalNumTriplets;
@@ -54,7 +57,7 @@ public class Triplets {
     private static Number getNumber(Long number, Long r) {
         Number cached = valuesCache.get(number);
         if(cached != null){
-            return new Number(cached.base, cached.rPower);
+            return cached;
         }
         Long base = number;
         Integer rPower = 0;
@@ -63,9 +66,9 @@ public class Triplets {
             base /= r;
             cached = valuesCache.get(base);
             if(cached != null){
-                Number triplet = new Number(cached.base, cached.rPower + rPower);
-                valuesCache.put(number, triplet);
-                return triplet;
+                rPower += cached.rPower;
+                base = cached.base;
+                break;
             }
         }
         Number triplet = new Number(base, rPower);
@@ -73,9 +76,27 @@ public class Triplets {
         return triplet;
     }
 
+    private static long countTripletsWhenBase1(List<Long> arr) {
+        final Map<Long, Integer> vals = new HashMap<>();
+        long totalSum = 0;
+        for (Long aLong : arr) {
+            vals.merge(aLong, 1, Integer::sum);
+            final Integer currentNumberOfVals = vals.get(aLong);
+            if(currentNumberOfVals > 2){
+                totalSum += countAriphmProgrSum(currentNumberOfVals - 2);
+            }
+        }
+
+        return totalSum;
+    }
+
+    private static long countAriphmProgrSum(int n){
+        return n*(n+1) / 2;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(".\\resources\\tripletsInput\\1_act.txt"));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(".\\resources\\tripletsInput\\act.txt"));
 
         String[] nr = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
