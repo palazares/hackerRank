463fc1a72621742918683668c034ad2a815d1f19
palazares <palazares@gmai.com>
2019-07-20 16:11:28 +0200

    backBone and cases for common child

---
+++ src/com/palazares/hackerrank/solutions/CommonChild.java
@@ -0,0 +1,23 @@
+package com.palazares.hackerrank.solutions;
+
+import java.util.Scanner;
+
+public class CommonChild {
+
+    static int commonChild(String s1, String s2) {
+
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+        scanner.close();
+
+        int result = commonChild(s1, s2);
+
+        System.out.println(result);
+    }
+}
