0479d45844c318421fd0d72df49ccffd7798ad70
Denis Sigaev <denis.sigaev@asml.com>
2019-07-26 18:24:03 +0200

    Working solution

--- src/com/palazares/hackerrank/solutions/ReverseShuffleMerge.java
+++ src/com/palazares/hackerrank/solutions/ReverseShuffleMerge.java
@@ -1,6 +1,5 @@
 package com.palazares.hackerrank.solutions;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,43 +8,25 @@ public class ReverseShuffleMerge {
     static String reverseShuffleMerge(String s) {
         final Map<Character, Integer> stringMap = stringMap(s);
         final Map<Character, Integer> stringMap2Build = stringMap2Build(stringMap);
-        Map<Character, Integer> accumulatedCharacters = new HashMap<>();
-        StringBuilder result = new StringBuilder();
         final char[] strChars = s.toCharArray();
-        for (int j = 0; j < strChars.length; j++) {
+        char[] stack = new char[s.length()];
+        int top = -1;
+        for (int j = strChars.length; --j >= 0; ) {
             Character c = strChars[j];
-            Integer charOccur = accumulatedCharacters.merge(c, 1, Integer::sum);
-            if (charOccur > stringMap2Build.get(c)) {
-                StringBuilder innerResult = new StringBuilder();
-                final Character minChar = Collections.min(accumulatedCharacters.keySet());
-                int i = j;
-                while (i >= 0 && strChars[i] != minChar) {
-                    if (strChars[i] == c) {
-                        if (charOccur == 1) {
-                            break;
-                        }
-                        charOccur--;
-                    }
-                    i--;
-                }
-                Character firstChar = strChars[i];
-
-                while (i >= 0 && stringMap2Build.getOrDefault(strChars[i], 0) > 0) {
-                    if (strChars[i] >= firstChar) {
-                        innerResult.append(strChars[i]);
-                        stringMap2Build.merge(strChars[i], 1, (prev, one) -> prev - one);
-                    }
-                    i--;
-                }
-
-                result.insert(0, innerResult.toString());
-                if (result.length() >= s.length() / 2) {
-                    return result.toString();
-                }
+            stringMap.merge(c, 1, (o, n) -> o - n);
+            if (stringMap2Build.getOrDefault(c, 0) <= 0) {
+                continue;
             }
+            stringMap2Build.merge(c, 1, (o, n) -> o - n);
+
+            while (top >= 0 && stack[top] > c &&
+                    stringMap.getOrDefault(stack[top], 0) > stringMap2Build.getOrDefault(stack[top], 0)) {
+                stringMap2Build.merge(stack[top--], 1, Integer::sum);
+            }
+            stack[++top] = c; // Push c on to the stack
         }
 
-        return result.toString();
+        return String.valueOf(stack).trim();
     }
 
     private static Map<Character, Integer> stringMap(String s) {
